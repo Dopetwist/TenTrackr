177b156c346e6e3d7f4861972b71bc7418be3f65
Jonathan <jonathanafugwobi1@gmail.com>
2026-03-17 01:59:06 +0100

    Server and endpoints setup

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+function Navbar() {
+    return (
+        <div>
+            <h1>TenTrackr Test</h1>
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ server/index.js
@@ -0,0 +1,76 @@
+import express from "express";
+import axios from "axios";
+import env from "dotenv";
+import cors from "cors";
+import pkg from "pg";
+
+const { Pool } = pkg;
+
+env.config();
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+// Database Connection
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+
+// Tenants endpoints
+app.get("/api/tenants", async (req, res) => {
+    try {
+        res.json([{id: 1, name: "John Doe"}, {id:2, name: "Jane Smith"}]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get("/api/tenants/:id", async (req, res) => {
+
+});
+
+app.post("/api/tenants", async (req, res) => {
+
+});
+
+app.put("/api/tenants/:id", async (req, res) => {
+
+});
+
+app.delete("/api/tenants/:id", async (req, res) => {
+
+});
+
+
+// Properties endpoints
+app.get("/api/properties", async (req, res) => {
+
+});
+
+app.post("/api/properties", async (req, res) => {
+
+});
+
+
+// Email endpoint
+app.post("/api/send-email", async (req, res) => {
+
+});
+
+
+// Document endpoint
+app.post("/api/upload-document", async (req, res) => {
+
+});
+
+
+// Server listener
+app.listen(port, () => {
+    console.log(`Backend server running on http://localhost:${port}`);
+});
