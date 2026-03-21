31cb50c8aec7d13d0357f849aa20b6408eb9263f
Jonathan <jonathanafugwobi1@gmail.com>
2026-03-21 19:36:42 +0100

    Work on Tenant registeration form

--- client/src/pages/Tenants.jsx
+++ client/src/pages/Tenants.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router";
 import PropertyAccordion from "../components/PropertyAccordion";
 
 function Tenants() {
+    const navigate = useNavigate();
+
     return (
         <div className="tenants">
             <h2>All Tenants</h2>
@@ -12,7 +15,11 @@ function Tenants() {
                 <input type="text" name="search" id="search" placeholder="Search tenant..." />
             </div>
             <div className="tenants-btns">
-                <button id="reg-tenant">Register Tenant</button>
+                <button id="reg-tenant"
+                onClick={() => navigate("/register")}
+                >
+                    Register Tenant
+                </button>
                 <button className="send-email">Send Email</button>
             </div>
 
