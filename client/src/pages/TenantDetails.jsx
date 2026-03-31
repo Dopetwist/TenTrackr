41204f39db87b70d090dda80b4a529a30b152248
Jonathan <jonathanafugwobi1@gmail.com>
2026-03-31 21:09:45 +0100

    Add delete Modal to TenantDetails component

--- client/src/pages/TenantDetails.jsx
+++ client/src/pages/TenantDetails.jsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useNavigate } from "react-router";
 import axios from "axios";
+import Modal from "../components/Modal";
 
 function TenantDetails() {
     const { id } = useParams();
 
     const [tenant, setTenant] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedTenantId, setSelectedTenantId] = useState(null);
+    const [ isDeleting, setIsDeleting ] = useState(false);
 
     const navigate = useNavigate();
 
@@ -49,6 +53,24 @@ function TenantDetails() {
     // Get symbol for tenant's currency, fallback to code if symbol not found
     const symbol = currencyMap[tenant.currency] || tenant.currency;
 
+    // Delete tenant from database
+    const handleDelete = async () => {
+        setIsDeleting(true);
+
+        try {
+            await axios.delete(`http://localhost:5000/api/tenants/${selectedTenantId}`);
+
+            navigate("/properties");
+
+            setShowModal(false);
+            setSelectedTenantId(null);
+        } catch (error) {
+            console.error("Error deleting tenant:", error.response?.data || error.message);
+        } finally {
+            setIsDeleting(false);
+        }
+    };
+
     return (
         <div id="details-container">
             <h2>Tenant Details</h2>
@@ -65,7 +87,16 @@ function TenantDetails() {
                             Edit Tenant
                         </button>
                         <button className="send-email">Send Email</button>
-                        <button className="delete-btn">Delete Tenant</button>
+                        <button 
+                        className="delete-btn"
+                        onClick={() => {
+                            console.log("Clicked");
+                            setSelectedTenantId(id);
+                            setShowModal(true);
+                        }}
+                        >
+                            Delete Tenant
+                        </button>
                     </div>
                 </div>
 
@@ -87,6 +118,17 @@ function TenantDetails() {
                     <p>- Payment Receipt</p>
                 </div>
             </div>
+
+            <Modal
+                isOpen={showModal}
+                title="Delete Tenant"
+                message="Are you sure you want to delete this tenant?"
+                onConfirm={handleDelete}
+                onCancel={() => setShowModal(false)}
+                confirmText="Delete"
+                cancelText="Cancel"
+                isLoading={isDeleting}
+            />
         </div>
     )
 }
